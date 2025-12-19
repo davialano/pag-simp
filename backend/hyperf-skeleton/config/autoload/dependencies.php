d3fe5557be841989dd5a97d8527dd9bb91265fd1
davialano <davialano11@gmail.com>
2025-12-18 22:39:37 -0300

    add: dependencies

--- backend/hyperf-skeleton/config/autoload/dependencies.php
+++ backend/hyperf-skeleton/config/autoload/dependencies.php
@@ -2,8 +2,24 @@
 
 declare(strict_types=1);
 
-use App\Repository\Usuario\UsuarioRepository;
-use App\Repository\Usuario\UsuarioRepositoryInterface;
+use App\Domain\Autorizador\AutorizadorInterface;
+use App\Domain\Notificacao\NotificacaoInterface;
+use App\Repository\Conta\Criar\CriarContaRepository;
+use App\Repository\Conta\Criar\CriarContaRepositoryInterface;
+use App\Repository\Conta\Depositar\DepositarContaRepository;
+use App\Repository\Conta\Depositar\DepositarContaRepositoryInterface;
+use App\Repository\Conta\Transferir\TransferirContaRepository;
+use App\Repository\Conta\Transferir\TransferirContaRepositoryInterface;
+use App\Repository\Log\LogTransacaoRepository;
+use App\Repository\Log\LogTransacaoRepositoryInterface;
+use App\Repository\Transacao\Depositar\DepositarTransacaoRepository;
+use App\Repository\Transacao\Depositar\DepositarTransacaoRepositoryInterface;
+use App\Repository\Transacao\Transferir\TransferirTransacaoRepository;
+use App\Repository\Transacao\Transferir\TransferirTransacaoRepositoryInterface;
+use App\Repository\Usuario\CriarUsuarioRepository;
+use App\Repository\Usuario\CriarUsuarioRepositoryInterface;
+use App\Service\Autorizador\AutorizadorService;
+use App\Service\Notificacao\NotificacaoService;
 
 /**
  * This file is part of Hyperf.
@@ -14,5 +30,21 @@ use App\Repository\Usuario\UsuarioRepositoryInterface;
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 return [
-    UsuarioRepositoryInterface::class => UsuarioRepository::class
+    CriarUsuarioRepositoryInterface::class => CriarUsuarioRepository::class,
+
+    CriarContaRepositoryInterface::class => CriarContaRepository::class,
+
+    DepositarContaRepositoryInterface::class => DepositarContaRepository::class,
+
+    DepositarTransacaoRepositoryInterface::class => DepositarTransacaoRepository::class,
+
+    LogTransacaoRepositoryInterface::class => LogTransacaoRepository::class,
+
+    TransferirContaRepositoryInterface::class => TransferirContaRepository::class,
+
+    TransferirTransacaoRepositoryInterface::class => TransferirTransacaoRepository::class,
+
+    AutorizadorInterface::class => AutorizadorService::class,
+
+    NotificacaoInterface::class => NotificacaoService::class
 ];
